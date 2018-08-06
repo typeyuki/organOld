7b268cebf97844651643c9383916df6c43603ebf
“wisher” <601011162@qq.com>
2018-08-06 15:31:17 +0800

    加了一个module visualService

---
+++ organOld-web/src/main/java/com/organOld/web/controller/VisualDataController.java
@@ -0,0 +1,14 @@
+package com.organOld.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 可视化 数据
+ * Created by netlab606 on 2018/6/25.
+ */
+@Controller
+@RequestMapping("/visual/data")
+public class VisualDataController {
+
+}
